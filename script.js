83979e2f73fa142740d8ac2b290b2a0cf2de17cf
Aleksei Buhtin <alekseibuhtin@mail.ru>
2021-10-24 20:46:48 +0300

    end lesson11

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+const btn = document.getElementById('btn')
+const eBtn = document.getElementById('e_btn')
+const square = document.getElementById('square')
+const circle = document.getElementById('circle')
+const rangeInput = document.getElementById('range')
+const span = document.querySelector('range-span')
+const textInput = document.getElementById('text')
+
+
+const newColor = function() {
+  square.style.backgroundColor = textInput.value 
+}
+
+btn.addEventListener('click', newColor)
+eBtn.style.display = 'none'
+
+rangeInput.addEventListener('input', function() {
+  circle.style.height = rangeInput.value + "%"
+  circle.style.width = rangeInput.value + "%"
+})
